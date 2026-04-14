3946e6c1ef5f82ea50d01a9ecfee54322f5c67e0
itcookiemin <miniya77@gmail.com>
2026-04-14 20:19:20 +0900

    cross origin 수정

--- src/main/java/io/itcookies/edu/controller/GeminiApiController.java
+++ src/main/java/io/itcookies/edu/controller/GeminiApiController.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/gemini")
 public class GeminiApiController {
